23164173b421e7c70c355e4e0cd786a79665a6c1
Tomoki-13 <s266016@wakayama-u.ac.jp>
2024-04-28 14:29:16 +0900

    add tsastfile

---
+++ src/utils/analyzetsAst.ts
@@ -0,0 +1,49 @@
+const parser = require("@babel/parser");
+import { promises as fsPromises } from 'fs';
+const traverse = require("@babel/traverse").default;
+
+export const analyzetsAst = async(allFiles: string[],libName:string): Promise<string[]> => {
+    let codes:string[]=[];
+    for (const filePath of allFiles) {
+        //ファイルの内容を取得
+
+        const fileContent:string = await fsPromises.readFile(filePath, 'utf8');
+        const parsed = parser.parse(fileContent, { ecmaVersion: 2020 ,sourceType: 'module',plugins: ["typescript"] });
+        //console.log(parsed);
+        traverse( parsed, {
+            ImportDeclaration(path:any) {
+                const node = path.node;
+                for (const specifier of node.specifiers) {
+                    if (specifier.local.type === 'Identifier' && specifier.local.name.includes(libName)) {
+                        const code = fileContent.substring(node.start, node.end);
+                        codes.push(code);
+                    }
+                }
+            },
+            VariableDeclaration(path:any) {
+                const node = path.node;
+                for (const declaration of node.declarations) {
+                    if (declaration.id.type === 'Identifier' && declaration.id.name.includes(libName)) {
+                        const code = fileContent.substring(node.start, node.end);
+                        codes.push(code);
+                    }
+                }
+            },
+            CallExpression(path:any) {
+                const node = path.node;
+                //関数の呼び出しを見つける
+                if (node.callee.type === 'Identifier' && node.callee.name === libName) {
+                    //UUID関数の呼び出しを見つけたら、そのノードを文字列に変換して保存
+                    const code:string = fileContent.substring(node.start, node.end);
+                    codes.push(code);
+                }else if(node.callee.type === 'MemberExpression'){
+                    if(node.callee.object && node.callee.object.type === 'Identifier' && node.callee.object.name === libName){
+                        const code:string = fileContent.substring(node.start, node.end);
+                        codes.push(code);
+                    }
+                }
+            }
+        });
+    }
+    return codes;
+}
