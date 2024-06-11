f858c1a225e509f94cc55f9280c381650dd901b3
Tomoki-13 <s266016@wakayama-u.ac.jp>
2024-06-11 19:44:24 +0900

    fix/getArgAst

--- src/utils/getArgAst.ts
+++ src/utils/getArgAst.ts
@@ -1,4 +1,4 @@
-const parser = require("@babel/parser");
+import * as parser from "@babel/parser";
 import { promises as fsPromises } from 'fs';
 import traverse from "@babel/traverse"; 
 import * as t from "@babel/types";
@@ -6,44 +6,39 @@ import * as t from "@babel/types";
 export const getArgAst = async(filePath:string,funcName:string): Promise<string[][]> => {
     let resultArray:string[][]=[];
     try {
-        let codes: string[] = [];
-        //ファイルの内容を取得
         if (filePath.endsWith('.js') || filePath.endsWith('.ts')) {
             const fileContent: string = await fsPromises.readFile(filePath, 'utf8');
             const parsed = parser.parse(fileContent, {sourceType: 'unambiguous', plugins: ["typescript",'decorators-legacy']});
-            //const parsed = parser.parse(fileContent, { ecmaVersion: 2020, sourceType: 'script', plugins: ["typescript"] });
             traverse(parsed, {
                 CallExpression(path: any) {
                     const node = path.node;
-                    // 関数の呼び出しを見つける
-                    if (node.callee.type === 'Identifier' && node.callee.name.includes(funcName)) {
+                    let codes: string[] = [];
+                    if (t.isIdentifier(node.callee) && node.callee.name.includes(funcName)) {
                         if (node.arguments.length > 0) {
                             const code = fileContent.substring(node.arguments[0].start, node.arguments[node.arguments.length - 1].end);
                             codes.push(code);
                         }
-                    } else if (node.callee.type === 'MemberExpression') {
-                        if (node.callee.object?.type === 'Identifier' && node.callee.object.name.includes(funcName)) {
+                    } else if (t.isMemberExpression(node.callee)) {
+                        if (t.isIdentifier(node.callee.object) && node.callee.object.name.includes(funcName)) {
                             if (node.arguments.length > 0) {
                                 const code = fileContent.substring(node.arguments[0].start, node.arguments[node.arguments.length - 1].end);
                                 codes.push(code);
                             }
-                        } else if (node.callee.object && node.callee.object.type === 'MemberExpression') {
-                            // ~~.default.~~()の取得
-                            if (node.callee.object.object && node.callee.object.object.type === 'Identifier') {
-                                if (node.callee.object.object.name.includes(funcName)) {
-                                    if (node.arguments.length > 0) {
-                                        const code = fileContent.substring(node.arguments[0].start, node.arguments[node.arguments.length - 1].end);
-                                        codes.push(code);
-                                    }
+                        } else if (t.isMemberExpression(node.callee)) {
+                            if (t.isIdentifier(node.callee.object.object) && node.callee.object.object.name.includes(funcName)) {
+                                if (node.arguments.length > 0) {
+                                    const code = fileContent.substring(node.arguments[0].start, node.arguments[node.arguments.length - 1].end);
+                                    codes.push(code);
                                 }
                             }
                         }
                     }
+                    if(codes.length > 0){
+                        resultArray.push(codes);
+                        codes = [];
+                    }
                 },
             });
-        }
-        if(codes.length > 0){
-            resultArray.push(codes);
         }
     } catch (error) {
         console.log(`Failed to create AST for file: ${filePath}`);
@@ -51,3 +46,4 @@ export const getArgAst = async(filePath:string,funcName:string): Promise<string[
     }
     return resultArray;
 }
+
