cb75b24dc1968e375f3182427eff58a48e4ecbed
Tomoki-13 <s266016@wakayama-u.ac.jp>
2024-06-11 19:39:37 +0900

    add/test

---
+++ src/__tests__/analyzeAst.test.ts
@@ -0,0 +1,12 @@
+import {analyzeAst} from '../utils/analyzeAst';
+describe('analyzeAst.ts test', () => {
+    test('Function usage', async () => {
+        const expectedOutput:string[] = ['sum(a,b)','sum(b,c)'];
+        await expect(analyzeAst('./src/__tests__/InputFile/sample.ts','sum')).resolves.toEqual(expectedOutput);
+    });
+    test('namespace usages', async () => {
+        const expectedOutput:string[] = ["FuncA.greet('World')", 'FuncA.add(2, 3)'];
+        //pathはsrcから
+        await expect(analyzeAst('./src/__tests__/InputFile/sample.ts','FuncA')).resolves.toEqual(expectedOutput);
+    });
+})
--- src/__tests__/getArgAst.test.ts
+++ src/__tests__/getArgAst.test.ts
@@ -1,6 +1,18 @@
 import {getArgAst} from '../utils/getArgAst';
-test('Extract arguments from source code', async () => {
-    const expectedOutput = [['a,b']];
-    //pathはsrcから
-    await expect(getArgAst('./src/__tests__/InputFile/sample.ts', 'sum')).resolves.toEqual(expectedOutput);
-});
+describe('getArgAst', () => {
+    test('no arguments', async () => {
+        const expectedOutput:string[][] = [];
+        //pathはsrcから
+        await expect(getArgAst('./src/__tests__/InputFile/sample.ts','Hello')).resolves.toEqual(expectedOutput);
+    });
+    test('no arguments', async () => {
+        const expectedOutput:string[][] = [["5,3"]];
+        //pathはsrcから
+        await expect(getArgAst('./src/__tests__/InputFile/sample.ts','subtract')).resolves.toEqual(expectedOutput);
+    });
+    test('two elements', async () => {
+        const expectedOutput:string[][] = [['a,b'],['b,c']];
+        //pathはsrcから
+        await expect(getArgAst('./src/__tests__/InputFile/sample.ts','sum')).resolves.toEqual(expectedOutput);
+    });
+})
