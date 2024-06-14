6d306ddb02fa8b0eded389be83854250bb37230c
Tomoki-13 <s266016@wakayama-u.ac.jp>
2024-06-14 15:20:20 +0900

    add/interface

---
+++ src/types/FunctionInfo.ts
@@ -0,0 +1,9 @@
+export interface FunctionInfo {
+    name: string;
+    args: string[];
+    isExported: boolean;
+}
+export interface ExportFunctionInfo {
+    name: string;
+    args: string[];
+}
