f262e3931ee8830bd1fb88ec7b22e9724c314ae0
Tomoki-13 <s266016@wakayama-u.ac.jp>
2024-06-14 15:21:53 +0900

    add/analyzeExpression and test

--- src/__tests__/analyzeAst.test.ts
+++ src/__tests__/analyzeAst.test.ts
@@ -1,4 +1,5 @@
-import {analyzeAst} from '../utils/analyzeAst';
+import {analyzeAst,analyzeExpression} from '../utils/analyzeAst';
+import { FunctionInfo} from '../types/FunctionInfo';
 describe('analyzeAst.ts test', () => {
     test('Function usage', async () => {
         const expectedOutput:string[] = ['sum(a,b)','sum(b,c)'];
@@ -8,5 +9,16 @@ describe('analyzeAst.ts test', () => {
         const expectedOutput:string[] = ["FuncA.greet('World')", 'FuncA.add(2, 3)'];
         //pathはsrcから
         await expect(analyzeAst('./src/__tests__/InputFile/sample.ts','FuncA')).resolves.toEqual(expectedOutput);
+    });
+})
+//analyzeExpression(filePath:string,funcName:string)
+describe('analyzeExpression test', () => {
+    test('analyzeExpression usage1', async () => {
+        const expectedOutput: FunctionInfo[] = [
+            { name: 'sum', args: [ 'a','b' ], isExported: true },
+            { name: 'subtract', args: [ 'a','b' ], isExported: false },
+            { name: 'Hello', args: [  ], isExported: false },
+        ];
+        await expect(analyzeExpression('./src/__tests__/InputFile/sample.ts', '')).resolves.toEqual(expectedOutput);
     });
 })
