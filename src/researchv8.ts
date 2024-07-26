966b5e0ed9b8b0f8fa82f818d1e5b24ab653f8f7
Tomoki-13 <s266016@wakayama-u.ac.jp>
2024-07-26 17:25:58 +0900

    add/csvOutputFunc

--- src/researchv8.ts
+++ src/researchv8.ts
@@ -2,6 +2,8 @@ import { getSubDir } from "./utils/getSubDir";
 import { getAllFiles } from "./utils/getAllFiles";
 const {add_extract,add_full_extract} = require("./utils/add_extract");
 import { useFunc } from "./utils/useFunc";
+import fs from 'fs';
+import path from 'path';
 
 (async () => {
     const startDirectory: string = "../reposv8.0.0success";
@@ -10,6 +12,7 @@ import { useFunc } from "./utils/useFunc";
     try {
         const libName: string | undefined = process.argv[2];
         const alldirs: string[] = await getSubDir(startDirectory);
+        const csvRows: string[] = ['client,Patterns']; 
         
         //各ディレクトリに対する処理
         for (const subdir of alldirs) {
@@ -28,11 +31,26 @@ import { useFunc } from "./utils/useFunc";
             //成功したクライアントでパターンを持っているものを検出
             if (extract_pattern.length !== 0) {
                 console.log(subdir);
-                console.log(extract_pattern);
+                //console.log(extract_pattern);
                 console.log(extract_pattern2);
                 n++;
+                //CSV行に追加
+                const patternString = JSON.stringify(extract_pattern2).replace(/"/g, '""');
+                csvRows.push(`"${subdir}","${patternString}"`);
             }
         }
+        const outputDirectory = path.resolve(__dirname, '../output');
+        if(!fs.existsSync(outputDirectory)) {
+            fs.mkdirSync(outputDirectory);
+        }
+        let outputFileName = path.join(outputDirectory, `${path.basename(startDirectory)}_preoutput.csv`);
+        //ファイルの重複阻止
+        if(fs.existsSync(outputFileName)) {
+            const date = new Date();
+            const formattedDate = date.toISOString().slice(0, 19).replace(/[T:]/g, '-');
+            outputFileName = path.join(outputDirectory, `${path.basename(startDirectory)}_output_${formattedDate}.csv`);
+        }
+        fs.writeFileSync(outputFileName, csvRows.join('\n'), 'utf8');
         console.log(alldirs.length);
         console.log(n);
     } catch (err) {
