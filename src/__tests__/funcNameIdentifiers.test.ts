a073ab71cf1249c91faec93d9ed814815d395dc7
Tomoki-13 <s266016@wakayama-u.ac.jp>
2024-08-29 13:04:38 +0900

    add/funcNameIdentifiers_test

--- src/__tests__/funcNameIdentifiers.test.ts
+++ src/__tests__/funcNameIdentifiers.test.ts
@@ -33,4 +33,36 @@ describe('analyzeAst.ts test', () => {
         const expectedOutput: string[] = ['FuncA', 'FuncB'];
         expect(result).toEqual(expectedOutput);
     });
+    test('import', async () => {
+        const filePath:string = "./src/__tests__/InputFile/importsample.js";
+        const libName:string = 'module';
+        const fileContent: string = await fsPromises.readFile(filePath, 'utf8');
+        const lines = extractImportLines(fileContent,libName);
+        let result:string[] = [];
+        for (const line of lines) {
+            let name:string[] = funcNameIdentifiers(line, libName);
+            if (name.length > 0) {
+                result = result.concat(name);
+            }
+        }
+        console.log(result);
+        const expectedOutput: string[] = ['a', 'v4', "v1","v5","b","c"];
+        expect(result).toEqual(expectedOutput);
+    });
+    test('require', async () => {
+        const filePath:string = "./src/__tests__/InputFile/requiresample.js";
+        const libName:string = 'module';
+        const fileContent: string = await fsPromises.readFile(filePath, 'utf8');
+        const lines = extractImportLines(fileContent,libName);
+        let result:string[] = [];
+        for (const line of lines) {
+            let name:string[] = funcNameIdentifiers(line, libName);
+            if (name.length > 0) {
+                result = result.concat(name);
+            }
+        }
+        console.log(result);
+        const expectedOutput: string[] = ['a', 'b', "v4","c","d","e"];
+        expect(result).toEqual(expectedOutput);
+    });
 })
