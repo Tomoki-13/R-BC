703998591ea57e7a9ca9e3cc3619bb6510ac20b7
Tomoki-13 <s266016@wakayama-u.ac.jp>
2024-11-17 22:36:53 +0900

    add/outputFunc,type

---
+++ src/types/outputTypes.ts
@@ -0,0 +1,20 @@
+export interface JsonRow {
+    failureclient: string;
+    detectPatterns: string[][];
+}
+
+export interface MatchClientPattern {
+    client: string;
+    pattern: string[][];
+    detectPattern: string[][];
+}
+
+export interface PatternCount {
+    pattern: string[][];
+    count: number;
+}
+
+export interface DetectionOutput {
+    patterns: PatternCount[];
+    totalCount: number;
+}
---
+++ src/utils/output_json.ts
@@ -0,0 +1,23 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+const createOutputDirectory = (dirPath: string): void => {
+    if(!fs.existsSync(dirPath)) {
+        //fs.mkdirSync(outputDirectory);
+        console.log(dirPath);
+    }
+};
+
+const getUniqueOutputPath = (baseDir: string, baseName: string, name: string): string => {
+    let outputPath = path.join(baseDir, `${baseName}_${name}.json`);
+    if(fs.existsSync(outputPath)) {
+        const date = new Date();
+        const formattedDate = date.toISOString().slice(0, 19).replace(/[T:]/g, '-');
+        outputPath = path.join(baseDir, `${baseName}_${name}_${formattedDate}.json`);
+    }
+    return outputPath;
+};
+export default {
+    createOutputDirectory,
+    getUniqueOutputPath
+};
