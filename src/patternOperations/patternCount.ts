1a6064000ab26f113cb3fdb3b022b035e83741e9
Tomoki-13 <s266016@wakayama-u.ac.jp>
2025-01-29 16:58:01 +0900

    add/combinePatterns

--- src/patternOperations/patternCount.ts
+++ src/patternOperations/patternCount.ts
@@ -1,5 +1,6 @@
+import { PatternCount } from "../types/outputTypes";
 //重複カウント
-export function countPatterns(pattern: string[][][]): { pattern: string[][], count: number }[] {
+export function countPatterns(pattern: string[][][]): PatternCount[] {
     const patternMap = new Map<string, number>();
     pattern.forEach(subrespattern => {
         const patternString = JSON.stringify(subrespattern);
@@ -14,4 +15,30 @@ export function countPatterns(pattern: string[][][]): { pattern: string[][], cou
         const pattern = JSON.parse(patternString);
         return { pattern, count };
     });
+};
+//パターンとカウントを結合 pattern1にpattern2を追加
+export function combinePatterns(pattern1:PatternCount[],pattern2:PatternCount[]): PatternCount[] {
+    const pattern1_Map = new Map<string, number>();
+    //pattern1のMap作成
+    for(const pattern1_i of pattern1) {
+        const patternString = JSON.stringify(pattern1_i.pattern);
+        if(pattern1_Map.has(patternString)){
+            console.log('combinePatterns_error');
+        }
+        pattern1_Map.set(patternString, pattern1_i.count);
+    }
+    
+    for(const pattern2_i of pattern2) {
+        const patternString = JSON.stringify(pattern2_i.pattern);
+        if(pattern1_Map.has(patternString)) {
+            pattern1_Map.set(patternString, pattern1_Map.get(patternString)! + pattern2_i.count);
+        } else {
+            pattern1_Map.set(patternString, pattern2_i.count);
+        }
+    }
+    let pattern:PatternCount[] = Array.from(pattern1_Map.entries()).map(([patternString, count]) => {
+        const pattern:string[][] = JSON.parse(patternString);
+        return { pattern, count };
+    });
+    return pattern.sort((a, b) => b.count - a.count);
 }
