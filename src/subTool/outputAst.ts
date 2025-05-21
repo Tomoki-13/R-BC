b2ecb8edbd8d18191bae3f4f6cd369fbd5f68af5
Tomoki-13 <s266016@wakayama-u.ac.jp>
2025-05-21 19:03:32 +0900

    add/outputAst

---
+++ src/subTool/outputAst.ts
@@ -0,0 +1,14 @@
+import { promises as fsPromises } from 'fs';
+const parser = require("@babel/parser");
+import path from 'path';
+import fs from 'fs';
+import output_json from "../utils/output_json";
+
+(async () => {
+    const filePath = '../__tests__/InputFile/argment/inFileSample1.ts';
+    const fileContent: string = await fsPromises.readFile(filePath, 'utf8');
+    const parsed = parser.parse(fileContent, { sourceType: 'unambiguous', plugins: ["typescript", 'decorators-legacy'] });
+    const outputDirectory = path.resolve(__dirname, './output');
+    output_json.createOutputDirectory(outputDirectory);
+    fs.writeFileSync(output_json.getUniqueOutputPath(outputDirectory,'astData','JSON'), JSON.stringify(parsed,null,2));
+})();
