27866500aaf800313a09f9914f17733aff7a8fbb
Tomoki-13 <s266016@wakayama-u.ac.jp>
2025-08-07 18:04:38 +0900

    特定のバージョン調査のコードのディレクトリ移動

--- src/specificAnalyze/detect_clients.ts
+++ src/specificAnalyze/detect_clients.ts
@@ -1,8 +1,8 @@
 import path from "path";
 import fs from 'fs';
-import output_json from "./utils/output_json";
-import { DetectionOutput} from "./types/outputTypes";
-import { detectByPattern } from "./combinations/detectByPattern";
+import output_json from "../utils/output_json";
+import { DetectionOutput} from "../types/outputTypes";
+import { detectByPattern } from "../combinations/detectByPattern";
 type dataType = {
     libName: string;
     pattrnListPath: string;
@@ -14,20 +14,20 @@ function extractAllPatterns(detectionOutput: DetectionOutput): string[][][] {
 }
 
 (async () => {
-    let absolutePath = path.resolve(__dirname, '../datasets/input/specific/specific_data.json');
+    let absolutePath = path.resolve(__dirname, '../../datasets/input/specific/specific_data.json');
     let data: dataType[] = JSON.parse(fs.readFileSync(absolutePath, 'utf-8'));
 
     const now = new Date();
     const date = output_json.formatDateTime(now);
 
     for(const {libName, pattrnListPath, targetDir} of data) {
-        let outputDir:string = path.resolve(process.cwd(), '../output/specific-' + date + '/' + libName);
+        let outputDir:string = path.resolve(process.cwd(), '../../output/specific/' + date + '/' + libName);
         let detect_outputDir = outputDir + '/detectByPattern';
         output_json.createOutputDirectory(detect_outputDir);
-        let patternListAndcount:DetectionOutput = JSON.parse(fs.readFileSync(path.resolve(__dirname, pattrnListPath), 'utf-8'));
+        let patternListAndcount:DetectionOutput = JSON.parse(fs.readFileSync(path.resolve(__dirname,'../' + pattrnListPath), 'utf-8'));
         let patternList:string[][][] = extractAllPatterns(patternListAndcount)
 
-        let matchCliantPatternJson:DetectionOutput = await detectByPattern( targetDir,libName,patternList,detect_outputDir,1);
+        let matchCliantPatternJson:DetectionOutput = await detectByPattern( '../' + targetDir,libName,patternList,detect_outputDir,1);
     }
    
 })();
