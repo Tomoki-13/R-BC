bc4404ec5c194e5f6af5858b53d06116a3a5d034
Tomoki-13 <s266016@wakayama-u.ac.jp>
2026-05-18 11:40:47 +0900

    ディレクトリではなくファイルの有無で実行するよう変更

--- scripts/clone_dataset.sh
+++ scripts/clone_dataset.sh
@@ -2,16 +2,19 @@
 
 set -Ceu
 
-cd  ..
-if [ -d "datasets" ]; then
-    echo "datasets are already cloned"
+cd ..
+
+if [ -f "datasets/test_result.json" ]; then
+    echo "test_result.json already exists"
     exit 0
 fi
 
 mkdir -p datasets
 
 git clone https://github.com/Wakayama-SocSEL/Matsuda.git ./datasets/Matsuda
-cp -rp ./datasets/Matsuda/output/test_result.json ./datasets/
+
+cp -p ./datasets/Matsuda/output/test_result.json ./datasets/
+
 rm -rf ./datasets/Matsuda
 
 echo "successfully cloned datasets"
